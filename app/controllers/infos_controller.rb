1768f77a01e7b021144f780f523cedd17c70eef3
RyoyaOzaki <r.ozaki3018@gmail.com>
2019-01-31 09:57:48 +0900

    completed

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,10 +1,17 @@
 class InfosController < ApplicationController
   def index
-    agent = Mechanize.new
-    page = agent.get('https://news.yahoo.co.jp/pickup/6311783')
-    @elements = {'date' => page.search('.date').inner_text, 
-                  'title' => page.search('#link').inner_text, 
-                  'abstruct' => page.search('.hbody').inner_text}
-    render :json => @elements
+    require 'rss'
+    @articles = []
+    filename = 'https://news.yahoo.co.jp/pickup/computer/rss.xml'
+    rss = RSS::Parser.parse(filename)
+    rss.items.each{|item|
+      agent = Mechanize.new
+      page = agent.get("#{item.link}")
+      @elements = {'date' => page.search('.date').inner_text, 
+                    'title' => page.search('#link').inner_text, 
+                    'abstruct' => page.search('.hbody').inner_text}
+      @articles << @elements
+    }
+    render :json => @articles
   end
 end
